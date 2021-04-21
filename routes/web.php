08658103bdcf8b95246b16ff954ee38eee3bb9fd
Gourav Sardana <gsardana001@gmail.com>
2021-04-21 11:59:09 +0530

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::view('form','userview');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('view-records','StudViewController@index');
-Route::any('search', 'UserSearchController@search');
+Route::any('search', 'UserSearchController@search')->middleware('auth');
 
 Route::post('submit','User@save');
 
